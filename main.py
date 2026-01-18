9ded89bf88e472254812a2c90359acf636133885
Greg Delgado <grd888@gmail.com>
2026-01-18 08:58:13 +0800

    add role to the prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -23,7 +24,7 @@ def generate_content(query: str) -> str:
     return response.text
 
 
-def print_metadata(query: str, response: genai.types.GenerateContentResponse) -> str:
+def print_metadata(query: str, response: types.GenerateContentResponse) -> str:
     """Prints metadata and content from a Gemini response.
 
     Args:
@@ -51,9 +52,8 @@ def main():
     parser = argparse.ArgumentParser(description="Chat with an AI assistant")
     parser.add_argument("user_prompt", type=str, help="The user prompt")
     args = parser.parse_args()
-
-    content = args.user_prompt
-    generate_content(content)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(messages)
 
 
 if __name__ == "__main__":
