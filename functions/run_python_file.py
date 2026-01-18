a8eb5e9ac555ea08b4c4bb9b0446e61918e21af7
Greg Delgado <grd888@gmail.com>
2026-01-18 11:24:21 +0800

    function to run a python file

---
+++ functions/run_python_file.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+from .utils import validate_path
+
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        target_dir, error = validate_path(working_directory, file_path, "x")
+        if error:
+            return error
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not target_dir.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        command = ["python", abs_file_path]
+        if args:
+            command.extend(args)
+
+        result = subprocess.run(command, capture_output=True, timeout=30.0, text=True)
+        if result.returncode != 0:
+            return f"Error: Process exited with code {result.returncode}"
+        if not result.stdout and not result.stderr:
+            return "No output produced"
+        output_str = ""
+        if result.stdout:
+            output_str += f"STDOUT: {result.stdout}\n"
+        if result.stderr:
+            output_str += f"STDERR: {result.stderr}\n"
+
+        return output_str
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- functions/utils.py
+++ functions/utils.py
@@ -13,7 +13,15 @@ def validate_path(
     try:
         abs_working_dir = os.path.abspath(working_directory)
         target_path = os.path.normpath(os.path.join(abs_working_dir, path))
-        err_str = "access" if mode == "r" else "write to"
+        match mode:
+          case "r":
+            err_str = "read"
+          case "w":
+            err_str = "write to"
+          case "x":
+            err_str = "execute"
+          case _:
+            return None, f'Error: Invalid mode: {mode}'
         if os.path.commonpath([abs_working_dir, target_path]) != abs_working_dir:
             return (
                 None,
